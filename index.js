e5e412c7e7ef1e304be755282597a197e7dcbfb2
rachedguitouni <119679014+rachedguitouni@users.noreply.github.com>
2023-09-09 21:11:00 +0200

    Create index.js
    create new file js for project

---
+++ index.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+//Example usage:
+const num1 = 5;
+const num2 = 10;
+const sum = addNumbers(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
